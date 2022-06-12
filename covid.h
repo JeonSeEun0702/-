c3de6859a91640871aacc8cf0ff75d9fae6f4f8f
전세은 <105336261+JeonSeEun0702@users.noreply.github.com>
2022-06-12 15:28:48 +0900

    Update covid.h

--- covid.h
+++ covid.h
@@ -5,5 +5,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 int find(int c);			//도시별 확진자 현황 함수
 int cpy(int n);				//방역수칙 함수
-int mid();					//확진자 평균 함수
+int mid();					  //확진자 평균 함수
 int cal(int i);				//연령별 백신 위험도 함수
