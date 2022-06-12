abfaef788b4e23c9cf38f96b666311818cad1a44
전세은 <105336261+JeonSeEun0702@users.noreply.github.com>
2022-06-12 15:41:04 +0900

    Update find.c

--- find.c
+++ find.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include "covid.h"
 
-static int ci[7] = { 100, 120, 80, 70, 90, 60, 50 };	//지역별 확진자 수(배열)
+static int ci[7] = { 100, 120, 80, 70, 90, 60, 50 };		//지역별 확진자 수(배열)
 
 int find(int c) {
 
 	for (int i = 0; i < 7; i++) {
-		if (ci[c - 1] == ci[i]) {	//배열 출력
+		if (ci[c - 1] == ci[i]) {			//배열 출력
 			printf("\n확진자는 %d명입니다.\n", ci[c - 1]);
 			printf("\n------------------------------\n\n");
 		}
