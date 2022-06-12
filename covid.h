0540f2ec190d6870c89af210b5b361b1450d03e3
전세은 <105336261+JeonSeEun0702@users.noreply.github.com>
2022-06-12 15:28:36 +0900

    Update covid.h

--- covid.h
+++ covid.h
@@ -1,9 +1,9 @@
-#pragma once  //컨파일을 한번만 실행
-#ifndef COVID.H   //다중소스 파일을 위한 헤더파일
+#pragma once				//컴파일을 한번만 실행
+#ifndef COVID.H				//다중소스 파일을 위한 헤더파일
 
 #endif
 #define _CRT_SECURE_NO_WARNINGS
-int find(int c);
-int cpy(int n);
-int mid();
-int cal(int i);
+int find(int c);			//도시별 확진자 현황 함수
+int cpy(int n);				//방역수칙 함수
+int mid();					//확진자 평균 함수
+int cal(int i);				//연령별 백신 위험도 함수
